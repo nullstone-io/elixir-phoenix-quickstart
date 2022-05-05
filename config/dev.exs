c8afde82ea520c3839351b835dd7631529ecdf9b
Brad Sickles <brad.sickles@gmail.com>
2022-05-05 11:44:47 -0400

    Finalize config

--- config/dev.exs
+++ config/dev.exs
@@ -2,10 +2,11 @@ import Config
 
 # Configure your database
 config :elixir_phoenix_quickstart, ElixirPhoenixQuickstart.Repo,
-  username: "postgres",
-  password: "postgres",
-  hostname: "localhost",
-  database: "elixir_phoenix_quickstart_dev",
+  url: System.get_env("DATABASE_URL"),
+#  username: "postgres",
+#  password: "postgres",
+#  hostname: "localhost",
+#  database: "elixir_phoenix_quickstart_dev",
   stacktrace: true,
   show_sensitive_data_on_connection_error: true,
   pool_size: 10
@@ -16,10 +17,11 @@ config :elixir_phoenix_quickstart, ElixirPhoenixQuickstart.Repo,
 # The watchers configuration can be used to run external
 # watchers to your application. For example, we use it
 # with esbuild to bundle .js and .css sources.
+
 config :elixir_phoenix_quickstart, ElixirPhoenixQuickstartWeb.Endpoint,
   # Binding to loopback ipv4 address prevents access from other machines.
   # Change to `ip: {0, 0, 0, 0}` to allow access from other machines.
-  http: [ip: {127, 0, 0, 1}, port: 4000],
+  http: [ip: {0, 0, 0, 0}, port: String.to_integer(System.get_env("PORT") || "4000")],
   check_origin: false,
   code_reloader: true,
   debug_errors: true,
